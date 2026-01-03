1d8804abf4316553628863ce71f2c1329798220d
Olu Bams <consult-me@olubams.com>
2026-01-03 12:56:55 -0500

    Added render to list of allowed cors

--- laundry-app-backend/app/main.py
+++ laundry-app-backend/app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:19006",
+        "https://your-render-app.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
