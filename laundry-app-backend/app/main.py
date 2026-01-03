3da65dfe43ed8f1dc3943ee48d0f5405baafac29
Olu Bams <consult-me@olubams.com>
2026-01-03 12:58:09 -0500

    minor update to main.py

--- laundry-app-backend/app/main.py
+++ laundry-app-backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:19006",
-        "https://your-render-app.onrender.com",
+        "https://laundroapp.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
